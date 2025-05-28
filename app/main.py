6ce7c37617a8687cd74843c462d4113faddb68da
Daniel Karnopp Schuh <danielkchuh@gmail.com>
2025-05-28 09:46:21 -0300

    resolved 2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 import functools
@@ -8,7 +8,7 @@ def cache(func: Callable) -> Callable:
     cache_storage = {}
 
     @functools.wraps(func)
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
         if key in cache_storage:
             print("Getting from cache")
